2626f17ff7aac14800368dcf3c7554dede172f25
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2024-08-01 11:39:53 -0400

    Only support interfaces for now

--- src/lib.rs
+++ src/lib.rs
@@ -54,105 +54,81 @@ pub enum Add {
 }
 
 pub fn transform(
-    mut package: wit_encoder::Package,
+    mut interface: wit_encoder::Interface,
     transforms: Vec<Transform>,
-) -> wit_encoder::Package {
+) -> wit_encoder::Interface {
     for transform in transforms {
         for operation in transform.operations {
             match operation {
-                Operations::RemoveType(item) => match &mut package.items_mut()[0] {
-                    wit_encoder::PackageItem::Interface(interface) => {
-                        interface.items_mut().retain(|i| match i {
-                            InterfaceItem::TypeDef(def) => def.name().as_ref() != item,
-                            InterfaceItem::Function(_) => true,
+                Operations::RemoveType(item) => {
+                    interface.items_mut().retain(|i| match i {
+                        InterfaceItem::TypeDef(def) => def.name().as_ref() != item,
+                        InterfaceItem::Function(_) => true,
+                    });
+                }
+                Operations::RemoveFunc { name, resource } => match resource {
+                    Some(resource) => {
+                        let type_def = find_type_def(&mut interface, &resource);
+                        let resource = match type_def.kind_mut() {
+                            wit_encoder::TypeDefKind::Resource(resource) => resource,
+                            _ => panic!("{resource}, is not a resource"),
+                        };
+                        resource.funcs_mut().retain(|f| match f.kind() {
+                            wit_encoder::ResourceFuncKind::Method(n, _) => n.to_string() != name,
+                            wit_encoder::ResourceFuncKind::Static(n, _) => n.to_string() != name,
+                            wit_encoder::ResourceFuncKind::Constructor => name != "constructor",
                         });
                     }
-                    wit_encoder::PackageItem::World(_) => todo!(),
-                },
-                Operations::RemoveFunc { name, resource } => match &mut package.items_mut()[0] {
-                    wit_encoder::PackageItem::Interface(interface) => match resource {
-                        Some(resource) => {
-                            let type_def = find_type_def(interface, &resource);
-                            let resource = match type_def.kind_mut() {
-                                wit_encoder::TypeDefKind::Resource(resource) => resource,
-                                _ => panic!("{resource}, is not a resource"),
-                            };
-                            resource.funcs_mut().retain(|f| match f.kind() {
-                                wit_encoder::ResourceFuncKind::Method(n, _) => {
-                                    n.to_string() != name
-                                }
-                                wit_encoder::ResourceFuncKind::Static(n, _) => {
-                                    n.to_string() != name
-                                }
-                                wit_encoder::ResourceFuncKind::Constructor => name != "constructor",
-                            });
-                        }
-                        None => {
-                            interface.items_mut().retain(|i| match i {
-                                InterfaceItem::TypeDef(_) => false,
-                                InterfaceItem::Function(func) => func.name().as_ref() != name,
-                            });
-                        }
-                    },
-                    wit_encoder::PackageItem::World(_) => todo!(),
+                    None => {
+                        interface.items_mut().retain(|i| match i {
+                            InterfaceItem::TypeDef(_) => false,
+                            InterfaceItem::Function(func) => func.name().as_ref() != name,
+                        });
+                    }
                 },
                 Operations::RemoveRecordField { record, field } => {
-                    match &mut package.items_mut()[0] {
-                        wit_encoder::PackageItem::Interface(interface) => {
-                            let record = find_record(interface, &record);
-                            // TODO: don't use retain_mut
-                            record
-                                .fields_mut()
-                                .retain_mut(|f| f.name().to_string() != field);
-                        }
-                        wit_encoder::PackageItem::World(_) => todo!(),
-                    }
+                    let record = find_record(&mut interface, &record);
+                    // TODO: don't use retain_mut
+                    record
+                        .fields_mut()
+                        .retain_mut(|f| f.name().to_string() != field);
                 }
                 Operations::RenameRecordField {
                     record: record_name,
                     old_field_name,
                     new_field_name,
-                } => match &mut package.items_mut()[0] {
-                    wit_encoder::PackageItem::Interface(interface) => {
-                        let old_field_name = Ident::new(old_field_name);
-                        let record = find_record(interface, &record_name);
-                        let field = record
-                            .fields_mut()
-                            .iter_mut()
-                            .find(|f| f.name() == &old_field_name)
-                            .expect(&format!("{record_name}.{old_field_name} not found"));
-                        field.set_name(new_field_name);
-                    }
-                    wit_encoder::PackageItem::World(_) => todo!(),
-                },
+                } => {
+                    let old_field_name = Ident::new(old_field_name);
+                    let record = find_record(&mut interface, &record_name);
+                    let field = record
+                        .fields_mut()
+                        .iter_mut()
+                        .find(|f| f.name() == &old_field_name)
+                        .expect(&format!("{record_name}.{old_field_name} not found"));
+                    field.set_name(new_field_name);
+                }
                 Operations::RetypeRecordField {
                     record: record_name,
                     field,
                     new_type,
-                } => match &mut package.items_mut()[0] {
-                    wit_encoder::PackageItem::Interface(interface) => {
-                        let field = Ident::new(field);
-                        let record = find_record(interface, &record_name);
-                        let field = record
-                            .fields_mut()
-                            .iter_mut()
-                            .find(|f| f.name() == &field)
-                            .expect(&format!("{record_name}.{field} not found"));
-                        field.set_type(new_type);
-                    }
-                    wit_encoder::PackageItem::World(_) => todo!(),
-                },
-                Operations::AddRecordField { record, field } => match &mut package.items_mut()[0] {
-                    wit_encoder::PackageItem::Interface(interface) => {
-                        let record = find_record(interface, &record);
-                        record.fields_mut().push(field);
-                    }
-                    wit_encoder::PackageItem::World(_) => todo!(),
-                },
+                } => {
+                    let field = Ident::new(field);
+                    let record = find_record(&mut interface, &record_name);
+                    let field = record
+                        .fields_mut()
+                        .iter_mut()
+                        .find(|f| f.name() == &field)
+                        .expect(&format!("{record_name}.{field} not found"));
+                    field.set_type(new_type);
+                }
+                Operations::AddRecordField { record, field } => {
+                    let record = find_record(&mut interface, &record);
+                    record.fields_mut().push(field);
+                }
                 Operations::Rename { from, to } => {
                     let from = Ident::new(from);
                     let to = Ident::new(to);
-                    visit_names_mut(&mut package, |name| {
+                    visit_names_mut(&mut interface, |name| {
                         if name == &from {
                             *name = to.clone();
                         }
@@ -161,24 +137,21 @@ pub fn transform(
                 Operations::ReplaceRefs { old, new } => {
                     let old = Ident::new(old);
                     let new = Ident::new(new);
-                    visit_refs_mut(&mut package, |name| {
+                    visit_refs_mut(&mut interface, |name| {
                         if name == &old {
                             *name = new.clone();
                         }
                     });
                 }
                 Operations::Add(add) => match add {
-                    Add::Type(ty) => match &mut package.items_mut()[0] {
-                        wit_encoder::PackageItem::Interface(interface) => {
-                            interface.items_mut().push(InterfaceItem::TypeDef(ty));
-                        }
-                        wit_encoder::PackageItem::World(_) => todo!(),
-                    },
+                    Add::Type(ty) => {
+                        interface.items_mut().push(InterfaceItem::TypeDef(ty));
+                    }
                 },
             }
         }
     }
-    package
+    interface
 }
 
 fn find_record<'a>(interface: &'a mut Interface, name: &str) -> &'a mut Record {
@@ -202,30 +175,23 @@ fn find_type_def<'a>(interface: &'a mut Interface, name: &str) -> &'a mut TypeDe
         .expect(&format!("Can't find type {name}"))
 }
 
-fn visit_names_mut<F>(package: &mut wit_encoder::Package, f: F)
+fn visit_names_mut<F>(interface: &mut wit_encoder::Interface, f: F)
 where
     F: Fn(&mut wit_encoder::Ident),
 {
-    for item in package.items_mut() {
+    for item in interface.items_mut() {
         match item {
-            wit_encoder::PackageItem::Interface(interface) => {
-                for item in interface.items_mut() {
-                    match item {
-                        InterfaceItem::TypeDef(ty) => {
-                            f(ty.name_mut());
-                        }
-                        InterfaceItem::Function(func) => {
-                            f(func.name_mut());
-                        }
-                    }
-                }
+            InterfaceItem::TypeDef(ty) => {
+                f(ty.name_mut());
             }
-            wit_encoder::PackageItem::World(_) => todo!(),
+            InterfaceItem::Function(func) => {
+                f(func.name_mut());
+            }
         }
     }
 }
 
-fn visit_refs_mut<F>(package: &mut wit_encoder::Package, f: F)
+fn visit_refs_mut<F>(interface: &mut wit_encoder::Interface, f: F)
 where
     F: Fn(&mut wit_encoder::Ident),
 {
@@ -237,61 +203,52 @@ where
             f(ty);
         }
     }
-
-    for item in package.items_mut() {
+    for item in interface.items_mut() {
         match item {
-            wit_encoder::PackageItem::Interface(interface) => {
-                for item in interface.items_mut() {
-                    match item {
-                        InterfaceItem::TypeDef(ty) => {
-                            match ty.kind_mut() {
-                                wit_encoder::TypeDefKind::Record(record) => {
-                                    for field in record.fields_mut() {
-                                        type_found(field.ty_mut(), &f);
-                                    }
-                                }
-                                wit_encoder::TypeDefKind::Resource(resource) => {
-                                    for func in resource.funcs_mut() {
-                                        for (_, ty) in func.params_mut().items_mut() {
-                                            type_found(ty, &f);
-                                        }
-                                    }
-                                }
-                                wit_encoder::TypeDefKind::Variant(variant) => {
-                                    for case in variant.cases_mut() {
-                                        if let Some(ty) = case.type_mut() {
-                                            type_found(ty, &f);
-                                        }
-                                    }
-                                }
-                                wit_encoder::TypeDefKind::Type(ty) => {
-                                    type_found(ty, &f);
-                                }
-                                wit_encoder::TypeDefKind::Flags(_)
-                                | wit_encoder::TypeDefKind::Enum(_) => {
-                                    // no types in flags/enums
-                                }
-                            }
+            InterfaceItem::TypeDef(ty) => {
+                match ty.kind_mut() {
+                    wit_encoder::TypeDefKind::Record(record) => {
+                        for field in record.fields_mut() {
+                            type_found(field.ty_mut(), &f);
                         }
-                        InterfaceItem::Function(func) => {
+                    }
+                    wit_encoder::TypeDefKind::Resource(resource) => {
+                        for func in resource.funcs_mut() {
                             for (_, ty) in func.params_mut().items_mut() {
                                 type_found(ty, &f);
                             }
-                            match func.results_mut() {
-                                wit_encoder::Results::Named(named) => {
-                                    for (_, ty) in named.items_mut() {
-                                        type_found(ty, &f);
-                                    }
-                                }
-                                wit_encoder::Results::Anon(ty) => {
-                                    type_found(ty, &f);
-                                }
+                        }
+                    }
+                    wit_encoder::TypeDefKind::Variant(variant) => {
+                        for case in variant.cases_mut() {
+                            if let Some(ty) = case.type_mut() {
+                                type_found(ty, &f);
                             }
                         }
                     }
+                    wit_encoder::TypeDefKind::Type(ty) => {
+                        type_found(ty, &f);
+                    }
+                    wit_encoder::TypeDefKind::Flags(_) | wit_encoder::TypeDefKind::Enum(_) => {
+                        // no types in flags/enums
+                    }
                 }
             }
-            wit_encoder::PackageItem::World(_) => todo!(),
+            InterfaceItem::Function(func) => {
+                for (_, ty) in func.params_mut().items_mut() {
+                    type_found(ty, &f);
+                }
+                match func.results_mut() {
+                    wit_encoder::Results::Named(named) => {
+                        for (_, ty) in named.items_mut() {
+                            type_found(ty, &f);
+                        }
+                    }
+                    wit_encoder::Results::Anon(ty) => {
+                        type_found(ty, &f);
+                    }
+                }
+            }
         }
     }
 }
--- tests/smoke.rs
+++ tests/smoke.rs
@@ -10,15 +10,29 @@ fn test(path: &str) {
     let mut packages = wit_encoder::packages_from_parsed(&resolve);
 
     assert!(packages.len() == 1, "Should create exactly one package");
-    let package = packages.remove(0);
-
-    let transforms =
-        fs::read_to_string(Path::new(&format!("./tests/{path}/transforms.json"))).unwrap();
-    let transforms: Vec<Transform> = serde_json::from_str(&transforms).unwrap();
-    let package = wit_transforms::transform(package, transforms);
+    let mut package = packages.remove(0);
+    assert!(
+        package.items().len() == 1,
+        "Package should contain exactly one item"
+    );
+    let item = package.items_mut().remove(0);
+    let interface = match item {
+        wit_encoder::PackageItem::Interface(interface) => interface,
+        wit_encoder::PackageItem::World(_) => panic!("Worlds not supported"),
+    };
+    let transforms = parse_json_file(format!("./tests/{path}/transforms.json"));
+    let interface = wit_transforms::transform(interface, transforms);
+    package.item(wit_encoder::PackageItem::Interface(interface));
 
     let expected = fs::read_to_string(Path::new(&format!("./tests/{path}/output.wit"))).unwrap();
     assert_eq!(expected, package.to_string());
+}
+
+fn parse_json_file(path: String) -> Vec<Transform> {
+    let transforms = fs::read_to_string(Path::new(&path)).unwrap();
+    let transforms = json_comments::StripComments::new(transforms.as_bytes());
+    let transforms = serde_json::from_reader(transforms).unwrap();
+    transforms
 }
 
 #[test]
